34d7033ee06472a264d5ab89c0512f8f05bb4550
ASK-DONE <knueppelalex@web.de>
2023-04-03 11:33:35 +0200

    030423

--- script_01.js
+++ script_01.js
@@ -3,22 +3,22 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 30;
-const ageMark = 20;
+// const ageJohn = 30;
+// const ageMark = 30;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+// let isJohnOlder, isJohnEqual;
 
-// Test : Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// // Test : Logische Aussage
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-console.log("ageJohn: " + ageJohn);
-console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// // Ausgabe
+// console.log("ageJohn: " + ageJohn);
+// console.log("ageMark: " + ageMark);
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
 
 
 //*********IF***************/
@@ -30,6 +30,95 @@ console.log("------------------");
 //if(isJohnOlder)
 //if(ageJohn>ageMark)
 
-{
-    console.log("John ist älter.")
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+//if (false)
+// if(isJohnOlder)
+// {
+//    console.log("John ist älter,"); 
+// } 
+// else
+//  {
+//     console.log("John ist jünger");
+// }
+
+/******  Ternäre (ternary) Schreibweise ********/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
+
+//**** IF - ELSE IF **********/
+// mit alternativen Fällen (älter, jünger, gleich alt)
+
+// if(isJohnOlder)
+// {
+//    console.log("John ist älter,"); 
+// } 
+// // 1...n Alternativen
+// else if(isJohnEqual)
+// {
+//     console.log("John ist gleich alt");
+// }
+// // wenn alle vorherigen Tests fehlschlagen...
+// else
+// {
+//     console.log("John ist jünger");
+// }
+
+
+/****** Fallunterscheidung / SWITCH|CASE 1 ******/
+
+const firstName = "Jane";
+let job;
+
+job = "driver";         // .. fährt TAXI! / UBER
+job = "diver";      // .. taucht im Rhein! 
+job = "artist";     // .. malt ein Bild!
+job = "pilot";      // .. macht etwas anderes! --> default
+job = "teacher";    // .. unterrichtet!
+job = "instructor";  // .. unterrichtet!
+
+// switch (job) {
+//     case "driver":
+//         console.log(firstName + " fährt TAXI!");
+//         // break;
+//     case "diver":
+//         console.log(firstName + " taucht im Rhein!");
+//         break;
+//     case "artist":
+//         console.log(firstName + " malt ein Bild!");
+//         break;
+//     case "teacher":      // geht auch so zu schreiben
+//     case "instructor":   // ausgabe ist nicht davon 
+//                          // abhängig was im case definiert ist
+//         console.log(firstName + " unterrichtet!");
+//         break;      
+        
+       
+//         default:
+//             console.log(firstName + " macht etwas anderes");
+//             break;
+// }
+
+/****** Fallunterscheidung / SWITCH|CASE 2 ******/
+// verwendbar für Tests oder Quize
+// AND / OR - && / ||
+
+
+
+const cond = true;
+const a = 1;
+
+switch (cond) {
+    case (a == 1):
+        console.log("a ist gleich 1")
+        break;
+    case (a == 2):
+        console.log("a ist gleich 2")
+        break;
+        
+        default:
+        console.log("a hat einen anderen Wert")
+        break;
 }
